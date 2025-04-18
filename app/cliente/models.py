2c40130c6b0cbb6a71da1c5f3a6c6fba0b4a38a0
Lucas Santos Pimentel <placte40@gmail.com>
2025-04-17 23:48:34 -0300

    implementando django, configurando Postgre e .env

---
+++ app/cliente/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Cliente(models.Model):
+    cli_id = models.AutoField(primary_key=True)
+    cli_nome = models.CharField(max_length=255)
+    cli_cpf = models.CharField(max_length=14, unique=True)
+    cli_cnpj = models.CharField(max_length=18, unique=True)
+    cli_rg = models.CharField(max_length=20, blank=True, null=True)
+    cli_dtnsc = models.DateField(blank=True, null=True)
+    cli_end = models.CharField(max_length=255, blank=True, null=True)
+    cli_end_num = models.CharField(max_length=10, blank=True, null=True)
+    cli_end_cid = models.CharField(max_length=100, blank=True, null=True)
+    cli_end_bairro = models.CharField(max_length=100, blank=True, null=True)
+    cli_tel = models.CharField(max_length=20, blank=True, null=True)
+    cli_cel = models.CharField(max_length=20, blank=True, null=True)
+    cli_email = models.CharField(max_length=100, blank=True, null=True)
+    cli_tipo = models.CharField(max_length=20, blank=True, null=True)
